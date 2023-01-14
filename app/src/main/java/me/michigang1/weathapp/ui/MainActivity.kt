5b184541673a96530a36cf00e3cfba98a3761ce0
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-01-14 04:12:22 +0100

    UI: created main layout for displaying general info of weather

--- app/src/main/java/me/michigang1/weathapp/ui/MainActivity.kt
+++ app/src/main/java/me/michigang1/weathapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package me.michigang1.weathapp
+package me.michigang1.weathapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.michigang1.weathapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
