f61538fb9272fde87f0bcbac8441a1e8e34befb8
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-01-14 04:47:31 +0100

    UI: created daily and hourly layouts. Also in MainActivity.kt support of MainFragment was added

--- app/src/main/java/me/michigang1/weathapp/ui/MainActivity.kt
+++ app/src/main/java/me/michigang1/weathapp/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package me.michigang1.weathapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import me.michigang1.weathapp.R
+import me.michigang1.weathapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, MainFragment.newInstance())
+                .commit()
+        }
     }
 }
