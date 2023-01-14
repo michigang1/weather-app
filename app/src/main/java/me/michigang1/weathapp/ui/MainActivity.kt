9a809d0699768f83b943d5f089f52ffe86eb8550
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-01-14 05:24:36 +0100

    UI: added toolbar and viewBinding delegating

--- app/src/main/java/me/michigang1/weathapp/ui/MainActivity.kt
+++ app/src/main/java/me/michigang1/weathapp/ui/MainActivity.kt
@@ -3,11 +3,21 @@ package me.michigang1.weathapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import me.michigang1.weathapp.R
+import me.michigang1.weathapp.databinding.ActivityMainBinding
 import me.michigang1.weathapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        toolbar = binding.toolbar
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
         if (savedInstanceState != null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_container, MainFragment.newInstance())
